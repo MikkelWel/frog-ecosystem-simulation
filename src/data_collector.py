6da3981a93c497e60e59f93beaa9448a3ac4dbd7
MikkelWel <mkaganw@gmail.com>
2026-03-25 00:58:48 -0400

    Add timestamps to simulation data collection

--- src/data_collector.py
+++ src/data_collector.py
@@ -1,8 +1,11 @@
 import csv
+import time
+from datetime import datetime
 
 class DataCollection:
     def __init__(self):
         self.step_history = []
+        self.timestamps = []
 
         self.population_history = []
         self.birth_history = []
@@ -16,6 +19,7 @@ class DataCollection:
 
     def record(self, step, frogs, food):
         self.step_history.append(step)
+        self.timestamps.append(datetime.now().isoformat())
         self.population_history.append(len(frogs))
         self.birth_history.append(self.births)
         self.death_history.append(self.deaths)
@@ -31,9 +35,10 @@ class DataCollection:
         with open(filename, "w", newline="") as file:
             writer = csv.writer(file)
 
-            writer.writerow(["Step", "Population", "Births", "Deaths", "Food Level", "Predator Kills"])
+            writer.writerow(["Timestamp", "Step", "Population", "Births", "Deaths", "Food Level", "Predator Kills"])
             for i in range(len(self.population_history)):
                 writer.writerow([
+                    self.timestamps[i],
                     self.step_history[i],
                     self.population_history[i],
                     self.birth_history[i],
